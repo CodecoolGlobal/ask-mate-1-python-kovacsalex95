66ddc6ebf22f50377460d514bcdff9247be73c83
kovacsalex <kovacsalex95@gmail.com>
2021-03-23 10:39:04 +0100

    model added

---
+++ model/answers.py
@@ -0,0 +1,82 @@
+DATA_PATH = 'data/question.csv'
+DATA_HEADERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+
+answers = {}
+'''
+
+{
+    [ANSWER_ID]: {
+        question_id: -1,
+        vote_number: 3
+        submission_time: ...
+        ...
+    }
+}
+
+'''
+
+
+
+def load_answers():
+    global answers
+    pass
+
+
+def save_answers(question_data):
+    # save questions
+
+    # load_questions()
+    pass
+
+
+def get_question_answers(question_id):
+    global answers
+
+    # return answers[?]
+
+    pass
+
+
+def get_answer(answer_id):
+    global answers
+
+    # return answers[?]
+
+    pass
+
+
+def add_answer(question_id, answer_data):
+    global answers
+
+    # add new answer
+
+    # save_answers(...)
+
+    # load_answers()
+
+    pass
+
+
+def edit_answer(answer_id, answer_data):
+    global answers
+
+    # edit answers[id] ...
+
+    # save_answers(...)
+
+    # load_answers()
+
+    pass
+
+
+def vote_answer(answer_id, vote):
+    # vote can be +1/-1
+    global answers
+
+    # edit answers[id] ...
+
+    # save_answers(...)
+
+    # load_answers()
+
+    pass
---
+++ model/questions.py
@@ -0,0 +1,63 @@
+DATA_PATH = 'data/question.csv'
+DATA_HEADERS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
+questions = {}
+'''
+
+{
+    [QUESTION_ID]: {
+        vote_number: 3
+        submission_time: ...
+        ...
+    }
+}
+
+'''
+
+def load_questions():
+    global questions
+    pass
+
+
+def save_questions(questions_data):
+    # save
+
+    # load_questions()
+    pass
+
+
+def get_questions():
+    global questions
+    return questions
+
+
+def get_questions(question_id):
+    global questions
+
+    # return questions[id]
+
+    pass
+
+
+def add_question(question_data):
+    global questions
+
+    # add new question
+
+    # save_questions(...)
+
+    # load_questions()
+
+    pass
+
+
+def edit_question(question_id, question_data)
+    global questions
+
+    # edit questions[id] ...
+
+    # save_questions(...)
+
+    # load_questions()
+
+    pass
